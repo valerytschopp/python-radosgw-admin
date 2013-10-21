88a353eb336a317ba477bfde00e6dc4004de5355
Valery Tschopp <valery.tschopp@switch.ch>
2013-10-21 11:26:19 +0200

    doc updated.

--- radosgw/user.py
+++ radosgw/user.py
@@ -30,8 +30,8 @@ class UserInfo(object):
 #  "keys":[
 #     {
 #      "user":"test",
-#      "access_key":"76A1O263ZTMJ6ECG0TLS",
-#      "secret_key":"cYWPiYJPS4feM2vdQY5iJJ1beZpuPEjN6+xGcY7t"
+#      "access_key":"7***S",
+#      "secret_key":"c***t"
 #      }
 #   ],
 #   "swift_keys":[],
@@ -47,10 +47,54 @@ class UserInfo(object):
 #   ]
 # }
 
-    def __init__(self,radosgw_connection,user_dict):
-        self._connection= radosgw_connection
+    def __init__(self, radosgw_admin, user_dict):
+        """INTERNAL ONLY.
+        :see: radosgw.connection.RadosGWAdminConnection:create_user()"""
+        self._rgwadmin = radosgw_admin
         for key in user_dict:
             self.__setattr__(key.lower(), user_dict[key])
-        
+
     def __str__(self):
-        return "<UserInfo: %s '%s'>" % (self.user_id,self.display_name)
+        return "<UserInfo: %s '%s'>" % (self.user_id, self.display_name)
+
+    def __repr__(self):
+        return "<UserInfo: %s '%s'>" % (self.user_id, self.display_name)
+
+    # display_name= None
+    # email= None
+    # key_type= 's3|swift',
+    # access_key= None
+    # secret_key= None
+    # user_caps= None
+    # generate_key= False
+    # max_buckets= None
+    # suspended= False
+    def update(self, **kwargs):
+        """Update the user.
+        :param str display_name: the display name
+        :param str email: the user email
+        :param str key_type: the key_type 's3' or 'swift'. Default: 's3'
+        :param str access_key: the access key
+        :param str secret_key: the secret key
+        :param bool generate_key: True to auto generate a new key pair. Default: False
+        :param str user_caps: the user caps. i.e. 'user=read'
+        :param int max_bucket: max bucket for the user
+        :param bool suspended: to suspend a user
+        :return radosgw.user.UserInfo: the updated user
+        :see: radosgw.connection.RadosGWAdminConnection#create_user
+        """
+        return self._rgwadmin.update_user(uid=self.user_id, **kwargs)
+
+    def delete(self, **kwargs):
+        """Delete the user.
+        :param bool purge_data: purge the user data. default: True
+        :returns bool:
+        """
+        return self._rgwadmin.delete_user(uid=self.user_id, **kwargs)
+
+    def get_buckets(self):
+        """Gets the user buckets information.
+        :return list: the list of buckets information
+        """
+        buckets = self._rgwadmin.get_buckets(uid=self.user_id)
+        return buckets

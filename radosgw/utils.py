94c186a85958f4d73973a11d0845191408aa1671
Valery Tschopp <valery.tschopp@switch.ch>
2017-02-20 10:41:57 +0100

    email address corrected

--- radosgw/utils.py
+++ radosgw/utils.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-# @author: Valery Tschopp <valery.tshopp@switch.ch>
+# author: Valery Tschopp <valery.tschopp@switch.ch>
 
 """Utilities"""
 

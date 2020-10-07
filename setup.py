64060cc928b4f464dfa9a73aba95a3283cad1b35
Valery Tschopp <valery.tschopp@switch.ch>
2020-10-07 09:25:06 +0200

    version 1.7.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except ImportError:
 
 setup(
     name='radosgw-admin',
-    version='1.7.1',
+    version='1.7.2',
     author='Valery Tschopp',
     author_email='valery.tschopp@gmail.com',
     include_package_data=True,
